cc38f67afa26bd83aa68ab88d4befd8e332c855f
U_Montag <montagu666@gmail.com>
2023-11-10 18:19:41 +0100

    Add cart page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,18 +1,22 @@
 import { Component } from '@angular/core';
 
+const ROWS_HEIGHT: { [id: number]: number } = { 1: 400, 3: 335, 4: 350 };
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  public cols: number = 3;
-  public category: string | undefined;
+  cols: number = 3;
+  rowHeight: number = 335;
+  category: string | undefined;
 
-  public onColumnsUpdated(colsNum: number): void {
+  onColumnsUpdated(colsNum: number): void {
     this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
   }
-  public onShowCategory(newCategory: string): void {
+  onShowCategory(newCategory: string): void {
     this.category = newCategory;
   }
+  onAddToCart(): void {}
 }
